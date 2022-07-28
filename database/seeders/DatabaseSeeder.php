45bb1b2d008c30ba1603873efd7282736269166b
afif1905 <afifbarca591@gmail.com>
2022-07-28 11:45:55 +0700

    EP Database Seeder & Factory Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,68 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
+
+        // User::create([
+        //     'name' => 'Afif',
+        //     'email' => 'afif@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        // User::create([
+        //     'name' => 'Heejin',
+        //     'email' => 'heejin@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
+
+        Category::create([
+            'name' => 'Web Programing',
+            'slug' => 'web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::factory(20)->create();
+
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sunt rem mollitia est saepe officiis animi, aut voluptatibus quas voluptas dolores earum corrupti doloremque qui non enim aspernatur aliquid ullam beatae. Fugiat, facilis voluptatum qui optio iure, ducimus, quibusdam temporibus ipsam error dolores beatae? Hic, eum magnam numquam veniam natus distinctio tenetur perferendis, aliquam, placeat quam odit iusto reiciendis doloremque nam vel. At cum repellendus cupiditate aliquid nostrum fuga consectetur quam. Aliquid qui recusandae amet tenetur, enim nam. Eum perferendis omnis ullam ea? Possimus a eum atque voluptas architecto, minima tempora ut dolore et, dolores dignissimos dolor totam soluta repellat?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sunt rem mollitia est saepe officiis animi, aut voluptatibus quas voluptas dolores earum corrupti doloremque qui non enim aspernatur aliquid ullam beatae. Fugiat, facilis voluptatum qui optio iure, ducimus, quibusdam temporibus ipsam error dolores beatae? Hic, eum magnam numquam veniam natus distinctio tenetur perferendis, aliquam, placeat quam odit iusto reiciendis doloremque nam vel. At cum repellendus cupiditate aliquid nostrum fuga consectetur quam. Aliquid qui recusandae amet tenetur, enim nam. Eum perferendis omnis ullam ea? Possimus a eum atque voluptas architecto, minima tempora ut dolore et, dolores dignissimos dolor totam soluta repellat?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sunt rem mollitia est saepe officiis animi, aut voluptatibus quas voluptas dolores earum corrupti doloremque qui non enim aspernatur aliquid ullam beatae. Fugiat, facilis voluptatum qui optio iure, ducimus, quibusdam temporibus ipsam error dolores beatae? Hic, eum magnam numquam veniam natus distinctio tenetur perferendis, aliquam, placeat quam odit iusto reiciendis doloremque nam vel. At cum repellendus cupiditate aliquid nostrum fuga consectetur quam. Aliquid qui recusandae amet tenetur, enim nam. Eum perferendis omnis ullam ea? Possimus a eum atque voluptas architecto, minima tempora ut dolore et, dolores dignissimos dolor totam soluta repellat?',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sunt rem mollitia est saepe officiis animi, aut voluptatibus quas voluptas dolores earum corrupti doloremque qui non enim aspernatur aliquid ullam beatae. Fugiat, facilis voluptatum qui optio iure, ducimus, quibusdam temporibus ipsam error dolores beatae? Hic, eum magnam numquam veniam natus distinctio tenetur perferendis, aliquam, placeat quam odit iusto reiciendis doloremque nam vel. At cum repellendus cupiditate aliquid nostrum fuga consectetur quam. Aliquid qui recusandae amet tenetur, enim nam. Eum perferendis omnis ullam ea? Possimus a eum atque voluptas architecto, minima tempora ut dolore et, dolores dignissimos dolor totam soluta repellat?',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,11 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By. Afif in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
     {!! $post->body !!}
     
 
-    <a href="/posts">Back to Posts</a>
+    <a href="/posts" class="d-block mt-3">Back to Posts</a>
 
 @endsection
