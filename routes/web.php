fe234b2d7fb8839a4080511f3beb277f330dce79
afif1905 <afifbarca591@gmail.com>
2022-07-31 11:36:33 +0700

    Searching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Heejin",
         "email" => "heejin@gmail.com",
         "image" => "heejin.jpg"
@@ -58,6 +60,7 @@ Route::get('/categories/{category:slug}', function(Category $category) {
 Route::get('/authors/{author:username}', function(User $author) {
     return view('posts', [
         'title' => "Post By Author : $author->name",
+        'active' => 'posts',
         'posts' => $author->posts->load('category', 'author')
     ]);
 });
