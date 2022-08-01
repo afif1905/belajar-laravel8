82200b283bd3626678d2559733e0f3e33f6e5f86
afif1905 <afifbarca591@gmail.com>
2022-08-01 10:51:54 +0700

    View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Models\Post;
-use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
-
-use App\Models\Category;
 use App\Models\User;
+use App\Models\Category;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -64,3 +66,8 @@ Route::get('/authors/{author:username}', function(User $author) {
         'posts' => $author->posts->load('category', 'author')
     ]);
 });
+
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
