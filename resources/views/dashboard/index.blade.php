62fe64bc95c843bedb138a0123a63304d127cde8
afif1905 <afifbarca591@gmail.com>
2022-08-04 10:55:46 +0700

    Dasboard UI

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,7 @@
-<h1>Welcome, afif</h1>
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Welcame back, {{ auth()->user()->name }}</h1>
+    </div>
+@endsection
